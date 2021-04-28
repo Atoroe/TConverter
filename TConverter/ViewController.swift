c410612f6f1f4e55567439057ae363e44e6ef966
Artiom P <apoluyanovich@gmail.com>
2021-04-28 15:44:35 +0300

    init main view

--- TConverter/ViewController.swift
+++ TConverter/ViewController.swift
@@ -9,11 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var celsiumLabel: UILabel!
+    @IBOutlet weak var farenheitLabel: UILabel!
+    
+
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        
     }
-
-
 }
 
