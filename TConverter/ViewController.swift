3ca1a5eec8d2c0355199a709f8b2bbb966945629
Artiom P <apoluyanovich@gmail.com>
2021-04-28 18:05:45 +0300

    added Celsius  to Fahrenheit converter algorithm

--- TConverter/ViewController.swift
+++ TConverter/ViewController.swift
@@ -11,10 +11,19 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var celsiumLabel: UILabel!
     @IBOutlet weak var farenheitLabel: UILabel!
+    @IBOutlet weak var temperatureSlider: UISlider! {
+        didSet {
+            temperatureSlider.minimumValue = 0
+            temperatureSlider.maximumValue = 100
+            temperatureSlider.value = 0
+        }
+    }
     
-
     @IBAction func sliderChanged(_ sender: UISlider) {
-        
+        let celsiumTemp = Int(round(sender.value))
+        celsiumLabel.text = "\(celsiumTemp)ºC"
+        let farenheitTemp = Int(round((sender.value * 9 / 5) + 32))
+        farenheitLabel.text = "\(farenheitTemp)ºF"
     }
 }
 
